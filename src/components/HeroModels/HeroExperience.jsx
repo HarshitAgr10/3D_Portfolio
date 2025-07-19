73e8075bd57ccf138e05b59b96a57d57edaeafb5
Harshit Agrawal <noreply@example.com>
2025-07-20 00:23:39 +0530

    Designed 3D Room in the Hero Section

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,6 +1,9 @@
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import {useMediaQuery} from "react-responsive";
+import {Room} from "./Room.jsx";
+import HeroLights from "./HeroLights.jsx";
+import Particles from "./Particles.jsx";
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
@@ -9,9 +12,6 @@ const HeroExperience = () => {
 
     return (
         <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-            <ambientLight intensity={0.2} color="#1a1a40" />
-            <directionalLight position={[5, 5, 5]} intensity={5} />
-
             <OrbitControls
                 enablePan={false}
                 enableZoom={!isTablet}
@@ -21,10 +21,22 @@ const HeroExperience = () => {
                 maxPolarAngle={Math.PI / 2}
             />
 
-        <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="teal" />
-        </mesh>
+        {/*<mesh>*/}
+        {/*    <boxGeometry args={[1, 1, 1]} />*/}
+        {/*    <meshStandardMaterial color="teal" />*/}
+        {/*</mesh>*/}
+
+            <HeroLights />
+
+            <Particles count={100} />
+
+            <group
+                scale={isMobile? 0.7 : 1}
+                position={[0, -3.5, 0]}
+                rotation={[0, -Math.PI / 4, 0]}
+            >
+                <Room />
+            </group>
         </Canvas>
 
     )
