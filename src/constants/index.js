5e7b1bd2efa1f1f48494f0e8593f489e540c57a3
Harshit Agrawal <noreply@example.com>
2025-07-20 23:31:56 +0530

    Added Title Animation, Scroll and Counter

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,6 +9,14 @@
     { text: "Code", imgPath: "/images/code.svg" },
 ];
 
+ const counterItems = [
+     { value: 15, suffix: "+", label: "Years of Experience" },
+     { value: 200, suffix: "+", label: "Satisfied Clients" },
+     { value: 108, suffix: "+", label: "Completed Projects" },
+     { value: 90, suffix: "%", label: "Client Retention Rate" },
+ ];
+
 export {
-    words
+    words,
+    counterItems
 };
