b14299daf03581dac2ce0d960e1bcc82df94f6b9
Harshit Agrawal <noreply@example.com>
2025-07-22 23:54:25 +0530

    Designed LogoShowcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './sections/Hero.jsx'
 import ShowCaseSection from "./sections/ShowCaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import LogoShowcase from "./components/LogoShowcase.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <NavBar/>
             <Hero />
             <ShowCaseSection />
+            <LogoShowcase/>
         </>
 
     )
